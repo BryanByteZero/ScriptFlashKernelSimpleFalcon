f3e01b1fd44c5d206502aa118415474642502a96
BryanByteZero <maxtrider@gmail.com>
2016-08-01 01:10:01 -0300

    Intial script based on a old from SGY mine because anykernel2 is not working now

---
+++ kernel/mkbootimg.sh
@@ -0,0 +1,6 @@
+#!/sbin/sh
+echo \#!/sbin/sh > /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage-dtb --ramdisk /tmp/newramdisk.cpio.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base 0x$(cat /tmp/boot.img-base) --pagesize 2048 --output /tmp/newboot.img >> /tmp/createnewboot.sh
+chmod 777 /tmp/createnewboot.sh
+/tmp/createnewboot.sh
+return $?
---
+++ kernel/repack-ramdisk.sh
@@ -0,0 +1,38 @@
+#!/sbin/sh
+
+# copy old kernel to sdcard
+if [ ! -e /sdcard/pre_pink_boot ]; then
+	cp /tmp/boot.img /sdcard/pre_zero_boot.img
+fi
+
+# decompress ramdisk
+mkdir /tmp/ramdisk
+cd /tmp/ramdisk
+gunzip -c ../boot.img-ramdisk.gz | cpio -i
+
+# add init.d support if not already supported
+found=$(find init.rc -type f | xargs grep -oh "run-parts /system/etc/init.d");
+if [ "$found" != 'run-parts /system/etc/init.d' ]; then
+        #find busybox in /system
+        bblocation=$(find /system/ -name 'busybox')
+        if [ -n "$bblocation" ] && [ -e "$bblocation" ] ; then
+                echo "BUSYBOX FOUND!";
+                #strip possible leading '.'
+                bblocation=${bblocation#.};
+        else
+                echo "NO BUSYBOX NOT FOUND! init.d support will not work without busybox!";
+                echo "Setting busybox location to /system/xbin/busybox! (install it and init.d will work)";
+                #set default location since we couldn't find busybox
+                bblocation="/system/xbin/busybox";
+        fi
+		#append the new lines for this option at the bottom
+        echo "" >> init.rc
+        echo "service userinit $bblocation run-parts /system/etc/init.d" >> init.rc
+        echo "    oneshot" >> init.rc
+        echo "    class late_start" >> init.rc
+        echo "    user root" >> init.rc
+        echo "    group root" >> init.rc
+fi
+
+find . | cpio -o -H newc | gzip > ../newramdisk.cpio.gz
+cd /
